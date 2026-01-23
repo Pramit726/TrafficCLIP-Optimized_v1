df05932cefd0ba4aadd8c5bac3662d45f2151c46
Pramit De <pramitde726@gmail.com>
2026-01-23 20:53:40 +0530

    fix: dataset path issue

--- src/train_runner.py
+++ src/train_runner.py
@@ -21,7 +21,7 @@ def run_experiment(args, config, device):
     # Set global seed for reproducibility
     set_seed(args.seed)
     train_loader, val_loader, _ = get_dataloader(
-        npz_path=config["paths"]["mini_output_data_file"],
+        npz_path=config["paths"]["output_data_file"],
         tokenizer=config["preprocess"]["tokenizer"],
         batch_size=config["preprocess"]["batch_size"],
         max_length=config["preprocess"]["max_length"],
